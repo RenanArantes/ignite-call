d97cc64e1ff3e322db17b086f0ed7a75681d6162
RenanArantes <malevo18@gmail.com>
2023-01-03 14:01:30 -0300

    instalação do Design System

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,11 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+// A função de estilo global é executada aqui mesmo pois precisa ser carregado apenas uma vez
+// então não necessariamento ela precisa estar dentro de um componente.
+// Se a função for colocada dentro do componente o NextJS recarregará novamente a cada reload da página.
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Heading } from '@ignite-ui/react'
+
 export default function Home() {
-  return <h1>Ignite Call</h1>
+  return <Heading as="h1">Ignite Call</Heading>
 }
